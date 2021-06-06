61091ad4e336f6553f74ca259cbf2819d4a658ec
ScottMilella <milella@hotmail.com>
2021-06-06 13:06:37 -0700

    Created Company scaffold and created CSV importer for Comapnies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_230942) do
+ActiveRecord::Schema.define(version: 2021_06_06_180527) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.string "status"
+    t.integer "terms"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.datetime "date"
